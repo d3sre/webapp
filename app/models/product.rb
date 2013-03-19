d40a9b3e6047a5809d86fc12e99aec5763c4f537
des <d.sacher@gmail.com>
2013-03-19 15:49:02 +0100

    first rails test until page 100 of book

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,11 @@
 class Product < ActiveRecord::Base
   attr_accessible :description, :image_url, :price, :title
-  validates_presence_of :title, :description, :price
+#  validates_presence_of :title, :description, :price
+  validates :title, :description, :price, :presence => true
+  validates :price, :numericality => {:greater_than_or_equal_to => 0.05}
+  validates :title, :uniqueness => true
+  validates :image_url, :format => { 
+	:with => %r{\.(gif|jpg|png)$}i, 
+	:message => 'must be a URL for GIF, JPG or PNG image'
+}
 end
