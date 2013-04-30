b4793c16e66a447c9d178631d82b084dae548da8
des <d.sacher@gmail.com>
2013-04-30 15:41:31 +0200

    iteration E2 finished

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -42,7 +42,8 @@ class LineItemsController < ApplicationController
   def create
     @cart = current_cart
     product = Product.find(params[:product_id])
-    @line_item = @cart.line_items.build(:product_id => product.id)
+    @line_item = @cart.add_product(product.id)
+    @line_item.product = product
 
     respond_to do |format|
       if @line_item.save
