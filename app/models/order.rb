55f5b23ae141c265b17e7eea60c4e79e8c08cb69
des <d.sacher@gmail.com>
2013-06-04 15:24:41 +0200

    fixed most issues - only duplicat add left

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,7 +1,8 @@
 class Order < ActiveRecord::Base
   has_many :line_items, :dependent => :destroy
-#  attr_accessible :address, :email, :name, :pay_type
+  attr_accessible :address, :email, :name, :pay_type
   PAYMENT_TYPES = [ "Check", "Credit Card", "Purchase order"]
+  validates_presence_of :name, :email, :pay_type
 
   def add_line_items_from_cart(cart)
     cart.line_items.each do |item|
